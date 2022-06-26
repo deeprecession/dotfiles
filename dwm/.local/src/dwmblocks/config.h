e5dca1aac1de26e2c27973ef7b0ee4824b2761fb
Vladislav Kishkovsky <67899905+aDeepRecession@users.noreply.github.com>
2022-06-26 18:18:49 +0300

    fix dwmblocks

--- dwm/.local/src/dwmblocks/config.h
+++ dwm/.local/src/dwmblocks/config.h
@@ -7,22 +7,17 @@ static const Block blocks[] = {
 	// {"",	"sb-music",	0,	11},
 	// {"",	"sb-pacpackages",	0,	8},
 	// {"",	"sb-news",		0,	6},
-	/* {"",	"sb-price lbc \"LBRY Token\" 📚",			9000,	22}, */
-	/* {"",	"sb-price bat \"Basic Attention Token\" 🦁",	9000,	20}, */
-	/* {"",	"sb-price link \"Chainlink\" 🔗",			300,	25}, */
-	/* {"",	"sb-price xmr \"Monero\" 🔒",			9000,	24}, */
-	/* {"",	"sb-price eth Ethereum 🍸",	9000,	23}, */
-	/* {"",	"sb-price btc Bitcoin 💰",				9000,	21}, */
-	{"| ",	"sb-memory",	10,	14},
-	{"| ",	"sb-cpu",		10,	18},
+	{"",	"sb-memory",	10,	14},
+	{"",	"sb-disk",		60,	21}, 
+	{"",	"sb-cpu",		10,	18},
 	/* {"",	"sb-moonphase",	18000,	17}, */
-	{"| ",	"sb-forecast",	18000,	5},
-	{"| ",	"sb-mailbox",	180,	12},
-	{"| ",	"sb-nettraf",	1,	16},
-	{"| ",	"sb-volume",	0,	10},
-	{"| ",	"sb-battery",	5,	3},
-	{"| ",	"sb-clock",	60,	1},
-	{"| ",	"sb-internet",	5,	4},
+	{"",	"sb-forecast",	18000,	5},
+	// {"",	"sb-mailbox",	180,	12},
+	{"",	"sb-nettraf",	1,	16},
+	{"",	"sb-volume",	0,	10},
+	{"",	"sb-battery",	5,	3},
+	{"",	"sb-clock",	1,	1},
+	{"",	"sb-internet",	5,	4},
 };
 
 //Sets delimiter between status commands. NULL character ('\0') means no delimiter.
