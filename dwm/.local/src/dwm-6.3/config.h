804e03edc173ef47d4edc6b412b1d4a60da9854c
Vladislav Kishkovsky <67899905+aDeepRecession@users.noreply.github.com>
2022-07-13 16:52:05 +0300

    fix: increase gaps

--- dwm/.local/src/dwm-6.3/config.h
+++ dwm/.local/src/dwm-6.3/config.h
@@ -8,8 +8,8 @@
 static unsigned int borderpx  = 3;        /* border pixel of windows */
 static unsigned int snap      = 32;       /* snap pixel */
 static unsigned int gappih    = 20;       /* horiz inner gap between windows */
-static unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
+static unsigned int gappiv    = 20;       /* vert inner gap between windows */
+static unsigned int gappoh    = 20;       /* horiz outer gap between windows and screen edge */
 static unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
 static int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
