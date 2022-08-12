4673db940f152c32d074e4af5ccc08f43c1349ac
Vladislav <aDeepRecession@noreply.github.com>
2022-08-12 09:07:45 +0300

    add debugger for c in nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -310,6 +310,31 @@ dap.listeners.after.event_initialized["dapui_config"] = function ()
    dapui.open() 
 end
 
+dap.configurations.cpp = {
+  {
+    name = 'Launch',
+    type = 'lldb',
+    request = 'launch',
+    program = function()
+      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+    end,
+    cwd = '${workspaceFolder}',
+    stopOnEntry = false,
+    args = {},
+    runInTerminal = false,
+  },
+}
+
+-- If you want to use this for Rust and C, add something like this:
+
+dap.configurations.c = dap.configurations.cpp
+dap.configurations.rust = dap.configurations.cpp
+dap.adapters.lldb = {
+  type = 'executable',
+  command = '/usr/bin/lldb-vscode', -- adjust as needed, must be absolute path
+  name = 'lldb'
+}
+
 dap.adapters.python = {
   type = 'executable';
   command = '/usr/bin/python';
