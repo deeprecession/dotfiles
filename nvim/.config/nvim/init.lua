23ae0b688d75d217e13a1fe3fcf7ef863605b0b5
Vladislav Kishkovsky <67899905+aDeepRecession@users.noreply.github.com>
2022-08-29 22:05:29 +0300

    add nvim-test plugin to nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -70,6 +70,9 @@ require('packer').startup(function(use)
     config = function() require("nvim-autopairs").setup {} end
   }
 
+  -- Test Runner
+  use "klen/nvim-test" 
+
 end)
 
 --WSL clipboard support
@@ -285,8 +288,31 @@ require('nvim-treesitter.configs').setup {
   },
 }
 
+require('nvim-test').setup {
+  run = true,                 -- run tests (using for debug)
+  commands_create = true,     -- create commands (TestFile, TestLast, ...)
+  filename_modifier = ":.",   -- modify filenames before tests run(:h filename-modifiers)
+  silent = false,             -- less notifications
+  term = "terminal",          -- a terminal to run ("terminal"|"toggleterm")
+  termOpts = {
+    direction = "vertical",   -- terminal's direction ("horizontal"|"vertical"|"float")
+    width = 96,               -- terminal's width (for vertical|float)
+    height = 24,              -- terminal's height (for horizontal|float)
+    go_back = false,          -- return focus to original window after executing
+    stopinsert = "auto",      -- exit from insert mode (true|false|"auto")
+    keep_one = true,          -- keep only one terminal for testing
+  },
+  runners = {               -- setup tests runners
+    go = "nvim-test.runners.go-test",
+    python = "nvim-test.runners.pytest",
+  }
+}
 
-
+vim.keymap.set("n", "<leader>tn", ":TestNearest<CR>")
+vim.keymap.set("n", "<leader>tf", ":TestFile<CR>")
+vim.keymap.set("n", "<leader>tl", ":TestLast<CR>")
+vim.keymap.set("n", "<leader>ts", ":TestSuite<CR>")
+vim.keymap.set("n", "<leader>tv", ":TestVisit<CR>")
 
 --Debugging setup
 vim.keymap.set("n", "<F5>", ":lua require'dap'.continue()<CR>")
