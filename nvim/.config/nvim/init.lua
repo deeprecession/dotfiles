a84aa66ea030a70189f8d96136c6b636b739949d
Vladislav <67899905+aDeepRecession@users.noreply.github.com>
2022-11-20 10:34:19 +0300

    update cmp config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -516,12 +516,16 @@ cmp.setup {
       luasnip.lsp_expand(args.body)
     end,
   },
+    window = {
+      -- completion = cmp.config.window.bordered(),
+      -- documentation = cmp.config.window.bordered(),
+    },
   mapping = cmp.mapping.preset.insert({
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<CR>'] = cmp.mapping.confirm {
-      -- behavior = cmp.ConfirmBehavior.Replace,
+      behavior = cmp.ConfirmBehavior.Insert,
       select = true,
     },
     ['<Tab>'] = cmp.mapping(function(fallback)
