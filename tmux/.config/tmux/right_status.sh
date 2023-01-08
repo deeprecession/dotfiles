688c408cd9758b6ee3fde9f3837571e6def93aae
Vladislav Kishkovskiy <67899905+aDeepRecession@users.noreply.github.com>
2023-01-08 23:03:53 +0300

    change tmux status bar

---
+++ tmux/.config/tmux/right_status.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+
+function battery_meter() {
+
+    if [ "$(which acpi)" ]; then
+
+        local icon=''
+
+        case $batt0 in
+
+            # From 100% to 75% display color grey.
+            100%|9[0-9]%|8[0-9]%|7[5-9]%) icon=''
+                ;;
+
+            # From 74% to 50% display color green.
+            7[0-4]%|6[0-9]%|5[0-9]%) icon=''
+                ;;
+
+            # From 49% to 25% display color yellow.
+            4[0-9]%|3[0-9]%|2[5-9]%) icon=''
+                ;;
+
+            # From 24% to 0% display color red.
+            2[0-4]%|1[0-9]%|[0-9]%) icon=''
+                ;;
+        esac
+
+        if [ "$(cat /sys/class/power_supply/AC/online)" == 1 ] ; then
+
+            local icon=''
+
+        fi
+
+        # Check for existence of a battery.
+        if [ -x /sys/class/power_supply/BAT1 ] ; then
+
+            local batt0=$(acpi -b 2> /dev/null | awk '/Battery 0/{print $4}' | cut -d, -f1)
+
+            # Display the percentage of charge the battery has.
+            printf "%s " "${icon} ${batt0}"
+
+        fi
+    fi
+}
+
+function load_average() {
+
+    printf "%s " "$(uptime | awk -F: '{printf $NF}' | tr -d ',')"
+
+}
+
+function date_time() {
+
+    printf "%s" "$(date +'%Y-%m-%d %H:%M:%S')"
+
+}
+
+function separator() {
+
+    printf " | "
+
+}
+
+function memory_usage() {
+
+    if [ "$(which bc)" ]; then
+
+        # Display used, total, and percentage of memory using the free command.
+        read used total <<< $(free -m | awk '/Mem/{printf $2" "$3}')
+        # Calculate the percentage of memory used with bc.
+        # percent=$(bc -l <<< "100 * $total / $used")
+        # Feed the variables into awk and print the values with formating.
+        awk -v u=$used -v t=$total -v p=$percent 'BEGIN {printf "Mem: %sMi/%sMi", t, u}'
+
+    fi
+
+}
+
+function main() {
+
+    separator
+    memory_usage
+    separator
+    battery_meter
+    # load_average
+
+}
+
+# Calling the main function which will call the other functions.
+main
