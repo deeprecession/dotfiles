32a5d3cbbc614ce6157ddde4bc73be161785b1a4
Vladislav Kishkovskiy <67899905+aDeepRecession@users.noreply.github.com>
2025-04-08 12:50:54 +0300

    add linters and add more events for linters to run

--- nvim/.config/nvim/lua/plugins/linting.lua
+++ nvim/.config/nvim/lua/plugins/linting.lua
@@ -9,17 +9,35 @@ return {
       local lint = require("lint")
 
       lint.linters_by_ft = {
+        html = { "htmlhint", "markuplint" },
+        json = { "jsonlint" },
         javascript = { "eslint_d" },
         typescript = { "eslint_d" },
         javascriptreact = { "eslint_d" },
         typescriptreact = { "eslint_d" },
         svelte = { "eslint_d" },
         python = { "pylint" },
+        go = { "golangci-lint", "nilaway" },
+        c = { "cpplint" },
+        cpp = { "cpplint" },
+        yaml = {"yamllint"},
+        sql = {"sqlfluff"},
+        protobuf = { "buf", "protolint" },
+        bash = { "shellcheck", "shellharden" },
       }
 
       vim.keymap.set("n", "<leader>l", function()
         lint.try_lint()
       end, { desc = "lint file" })
+
+      vim.api.nvim_create_autocmd({ "BufNewFile", "BufReadPost",
+        "BufReadPre", "BufWritePost",
+        "TextChanged", "TextChangedI" }, {
+        callback = function()
+          require("lint").try_lint()
+        end,
+      })
+
     end,
   },
 }
