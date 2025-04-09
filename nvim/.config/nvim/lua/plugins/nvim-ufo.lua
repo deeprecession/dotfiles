1fa8eb2357868df0316e57ace17b1e7a0d2222d8
Vladislav Kishkovskiy <67899905+aDeepRecession@users.noreply.github.com>
2025-04-09 20:31:22 +0300

    add desc to nvim-ufo keymaps

--- nvim/.config/nvim/lua/plugins/nvim-ufo.lua
+++ nvim/.config/nvim/lua/plugins/nvim-ufo.lua
@@ -3,9 +3,9 @@ return {
     "kevinhwang91/nvim-ufo",
     dependencies = "kevinhwang91/promise-async",
     config = function()
-      vim.keymap.set("n", "zR", require("ufo").openAllFolds)
-      vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
-      vim.keymap.set("n", "zr", require("ufo").openFoldsExceptKinds)
+      vim.keymap.set("n", "zR", require("ufo").openAllFolds, { desc = "open all folds" })
+      vim.keymap.set("n", "zM", require("ufo").closeAllFolds, { desc = "close all folds" })
+      vim.keymap.set("n", "zr", require("ufo").openFoldsExceptKinds, { desc = "open folds except kinds" })
     end,
   },
 }
