40052d7880afc228bace2e2d62948cfccda77aa0
Vladislav Kishkovskiy <67899905+aDeepRecession@users.noreply.github.com>
2025-05-07 14:31:37 +0300

    move to vim.lsp.config from mason_lsp

--- nvim/.config/nvim/lua/config/lsp/setup.lua
+++ nvim/.config/nvim/lua/config/lsp/setup.lua
@@ -1,7 +1,6 @@
 -- Setup installer & lsp configs
 local mason = require("mason")
 local mason_lsp = require("mason-lspconfig")
-local lspconfig = require("lspconfig")
 
 mason.setup({
   ui = {
@@ -58,30 +57,7 @@ function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
   return orig_util_open_floating_preview(contents, syntax, opts, ...)
 end
 
-require("mason-lspconfig").setup_handlers {
-  -- The first entry (without a key) will be the default handler
-  -- and will be called for each installed server that doesn't have
-  -- a dedicated handler.
-  function(server_name)
-    require("lspconfig")[server_name].setup {
-      on_attach = on_attach,
-      capabilities = capabilities,
-      handlers = handlers,
-    }
-  end,
-
-  -- ["ts_ls"] = function()
-  --   require("typescript-tools").setup({
-  --     capabilities = capabilities or vim.lsp.protocol.make_client_capabilities(),
-  --     handlers = require("config.lsp.servers.tsserver").handlers,
-  --     on_attach = require("config.lsp.servers.tsserver").on_attach,
-  --     settings = require("config.lsp.servers.tsserver").settings,
-  --     filetypes = require("config.lsp.servers.tsserver").filetypes,
-  --   })
-  -- end,
-
-  ["ts_ls"] = function()
-    lspconfig.ts_ls.setup({
+vim.lsp.config('ts_ls', {
       capabilities = capabilities,
       handlers = handlers,
       filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
@@ -95,48 +71,28 @@ require("mason-lspconfig").setup_handlers {
         },
       },
       settings = {
-      typescript = {
-        tsserver = {
-          useSyntaxServer = false,
-        },
-        inlayHints = {
-          -- includeInlayParameterNameHints = 'all',
-          -- includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-          -- includeInlayFunctionParameterTypeHints = false,
-          -- includeInlayVariableTypeHints = false,
-          -- includeInlayVariableTypeHintsWhenTypeMatchesName = false,
-          -- includeInlayPropertyDeclarationTypeHints = false,
-          -- includeInlayFunctionLikeReturnTypeHints = false,
-          -- includeInlayEnumMemberValueHints = true,
+        typescript = {
+          tsserver = {
+            useSyntaxServer = false,
+          },
+          inlayHints = {},
         },
       },
-    },
-    })
-  end,
+})
 
-  -- ["tailwindcss"] = function()
-  --   capabilities.textDocument.completion.completionItem.snippetSupport = true
-  --   capabilities.textDocument.colorProvider = { dynamicRegistration = false }
-  --   capabilities.textDocument.foldingRange = {
-  --     dynamicRegistration = false,
-  --     lineFoldingOnly = true,
-  --   }
-  --
-  --   lspconfig.tailwindcss.setup({
-  --     capabilities = capabilities,
-  --     filetypes = require("config.lsp.servers.tailwindcss").filetypes,
-  --     handlers = handlers,
-  --     init_options = require("config.lsp.servers.tailwindcss").init_options,
-  --     on_attach = require("config.lsp.servers.tailwindcss").on_attach,
-  --     settings = require("config.lsp.servers.tailwindcss").settings,
-  --     flags = {
-  --       debounce_text_changes = 1000,
-  --     },
-  --   })
-  -- end,
+vim.lsp.config('tailwindcss', {
+      capabilities = capabilities,
+      filetypes = require("config.lsp.servers.tailwindcss").filetypes,
+      handlers = handlers,
+      init_options = require("config.lsp.servers.tailwindcss").init_options,
+      on_attach = require("config.lsp.servers.tailwindcss").on_attach,
+      settings = require("config.lsp.servers.tailwindcss").settings,
+      flags = {
+        debounce_text_changes = 1000,
+      },
+})
 
-  ["cssls"] = function()
-    lspconfig.cssls.setup({
+vim.lsp.config('cssls', {
       capabilities = capabilities,
       handlers = handlers,
       on_attach = require("config.lsp.servers.cssls").on_attach,
@@ -144,11 +100,9 @@ require("mason-lspconfig").setup_handlers {
       flags = {
         debounce_text_changes = 1000,
       },
-    })
-  end,
+})
 
-  ["eslint"] = function()
-    lspconfig.eslint.setup({
+vim.lsp.config('eslint', {
       capabilities = capabilities,
       handlers = handlers,
       on_attach = require("config.lsp.servers.eslint").on_attach,
@@ -158,35 +112,27 @@ require("mason-lspconfig").setup_handlers {
         debounce_text_changes = 1000,
         exit_timeout = 1500,
       },
-    })
-  end,
+})
 
-  ["jsonls"] = function()
-    lspconfig.jsonls.setup({
+vim.lsp.config('jsonls', {
       capabilities = capabilities,
       handlers = handlers,
       on_attach = on_attach,
       settings = require("config.lsp.servers.jsonls").settings,
-    })
-  end,
+})
 
-  ["lua_ls"] = function()
-    lspconfig.lua_ls.setup({
+vim.lsp.config('lua_ls', {
       capabilities = capabilities,
       handlers = handlers,
       on_attach = on_attach,
       settings = require("config.lsp.servers.lua_ls").settings,
-    })
-  end,
+})
 
-  ["volar"] = function()
-    lspconfig.volar.setup({
+vim.lsp.config('volar', {
       filetypes = require("config.lsp.servers.volar").filetypes,
       handlers = handlers,
       capabilities = capabilities,
       init_options = require("config.lsp.servers.volar").init_options,
       on_attach = on_attach,
       settings = require("config.lsp.servers.volar").settings,
-    })
-  end
-}
+})
